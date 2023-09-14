88ad4098798492bc211c02fb37ed2f0ec78689e7
iyouseayyeye <133824790+iyouseayyeye@users.noreply.github.com>
2023-09-14 01:40:31 -0400

    fixed errors identified
    fixed errors for

--- App.tsx
+++ App.tsx
@@ -1,144 +1,157 @@
 import React, { useState, useEffect, useRef } from 'react';
-import {  View,  TextInput,  Button,  FlatList,  Text,  StyleSheet,  Image,  KeyboardAvoidingView,
-    Platform, TouchableOpacity,} from 'react-native';
+import { View, TextInput, Button, FlatList, Text, StyleSheet, Image, KeyboardAvoidingView, Platform, TouchableOpacity } from 'react-native';
 import { apiKey } from './config';
 import Tts from 'react-native-tts';
 import isSpeaking from 'react-native-tts';
 import Voice from '@react-native-community/voice';
 
-
 type Message = {
-  role: string;
-  content: string;
+    role: string;
+    content: string;
 };
 
-
 const App = () => {
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [input, setInput] = useState('');
-  const [isRecording, setIsRecording] = useState(false);
-  const [defaultLanguage, setDefaultLanguage] = useState('en-US');
+    const [messages, setMessages] = useState<Message[]>([]);
+    const [input, setInput] = useState('');
+    const [isRecording, setIsRecording] = useState(false);
+    const [defaultLanguage, setDefaultLanguage] = useState('en-US');
+    const flatListRef = useRef<FlatList>(null);
 
-  const flatListRef = useRef<FlatList>(null);
+    useEffect(() => {
+        // Add dummy event listeners
+        Voice.onSpeechStart = () => { };
+        Voice.onSpeechEnd = () => { };
 
-  useEffect(() => {
-    const initializeVoice = async () => {
-      try {
-        await Voice.start(defaultLanguage);
-      } catch (error) {
-        console.error("Failed to initialize Voice:", error);
-      }
+        // Cleanup on unmount
+        return () => {
+            Voice.onSpeechStart = () => { };
+            Voice.onSpeechEnd = () => { };
+        };
+    }, []);
+
+    useEffect(() => {
+        const initializeVoice = async () => {
+            try {
+                await Voice.start(defaultLanguage);
+            } catch (error) {
+                console.error("Failed to initialize Voice:", error);
+            }
+        };
+
+        initializeVoice();
+
+        const language = input.toLowerCase().includes('language') ? input : 'en-US';
+        Voice.start(language);
+        Voice.onSpeechResults = handleSpeechResults;
+        restartConversation();
+    }, []);
+
+    const handleSpeechResults = (e: { value?: string[] }) => {
+        if (e.value && e.value.length > 0) {
+            setInput(e.value[0]);
+            setIsRecording(false);
+        }
     };
-  
-    initializeVoice();
-  
-    const language = input.toLowerCase().includes('language') ? input : 'en-US'; // Assuming 'en-US' as your default language
-    Voice.start(language); // Set language for STT
 
-    const handleSpeechResults = (e: {value?: string[]}) => {
-      if (e.value) {
-        // handle the case where value is a string array
-      } else {
-        // handle the case where value is undefined
-      }
-    }
-
-    Voice.onSpeechResults = handleSpeechResults; // Callback for STT results
-    restartConversation();
-}, []);
-
-  const handleSpeechResults = (e: { value: string[] }) => {
-    setInput(e.value[0]); // Set input with STT result
-    setIsRecording(false); // Stop recording
-  };
-
-  const startRecording = async () => {
-    setIsRecording(true);
-    setInput('');
-    try {
-      await Voice.start(defaultLanguage);
-    } catch (error) {
-      console.error('Error starting speech recognition:', error);
-    }
-  };
-
-  const stopRecording = async () => {
-    setIsRecording(false);
-    try {
-      await Voice.stop();
-    } catch (error) {
-      console.error('Error stopping speech recognition:', error);
-    }
-  };
-
-  const sendMessage = async () => {
-    if (input) {
-      setMessages((prevMessages) => [...prevMessages, { role: 'user', content: input }]);
-      const lastMessages = messages.slice(Math.max(messages.length - 5, 0));
-
-      const systemMessage1 = 'You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.';
-      const systemMessage2 = 'REQUEST_PERSONA_CREATION (NAME:"Max the Appliance Pro", FOCUS:"AI assistant for appliance repair", BIO:"An AI assistant with 8 skills including comprehensive diagnostics, safety practices, and staying up-to-date with industry trends. Adapts to users\' unique needs and avoids 9 no-nos.", SKILLS:{1:"Comprehensive knowledge of diagnostics and repair techniques.",2:"Familiarity with appliance brands, models, and common issues.",3:"Step-by-step guidance for troubleshooting and repair.",4:"Expertise in recommending suitable parts and tools.",5:"Clear, patient, and effective communication.",6:"Adaptability to various skill levels and learning styles.",7:"Dedication to staying up-to-date with industry trends.",8:"Strong emphasis on safety guidelines."}, NO_NOS:{1:"Providing inaccurate or misleading information.",2:"Encouraging unsafe actions.",3:"Ignoring users\' skill levels and experience.",4:"Demonstrating impatience or frustration.",5:"Promoting unreliable or untested repair methods.",6:"Overlooking essential safety guidelines.",7:"Inability to adapt to different user needs.",8:"Offering unsolicited or irrelevant advice.",9:"Do not switch from GPT-4 to a default model."}, TEMPLATE:"AI persona Max the Appliance Pro for appliance repair industry. Adapts to users\' unique needs and avoids unreliable repair methods and safety violations.", INSTRUCTIONS:"Create AI persona Max the Appliance Pro for appliance repair industry with 8 skills, including comprehensive diagnostics, expertise in parts and tools, and a strong emphasis on safety. Avoids 9 no-nos, prioritizes adapting to each user\'s unique needs for optimal ability enhancement and high-quality user experience."';
-      const systemMessage3 = 'Using the provided make, model, and serial number, access available information and resources to provide a thorough and complete diagnosis of the home appliance. Identify common issues and suggest possible solutions based on the appliance\'s specific information. Include estimated likelihood percentages for each identified issue. Include detailed and comprehensive guide to locate, access, test, diagnose, and repair the identified parts. Include factory and aftermarket part numbers';
-
-      const chatMessages = [
-        { role: 'system', content: systemMessage1 },
-        { role: 'system', content: systemMessage2 },
-        { role: 'system', content: systemMessage3 },
-        ...lastMessages,
-        { role: 'user', content: input }
-      ];
-
-      try {
-        const response = await fetch('https://api.openai.com/v1/chat/completions', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`,
-          },
-          body: JSON.stringify({
-            model: 'gpt-4',
-            messages: chatMessages,
-          }),
-        });
-
-        const data = await response.json();
-        const botMessage = data.choices[0].message.content;
-        setMessages((prevMessages) => [...prevMessages, { role: 'assistant', content: botMessage }]);
+    const startRecording = async () => {
+        setIsRecording(true);
         setInput('');
-        
-      } catch (error) {
-        console.error('Error communicating with the GPT-4 API:', error);
-      }
-    }
-  };
+        try {
+            await Voice.start(defaultLanguage);
+        } catch (error) {
+            console.error('Error starting speech recognition:', error);
+        }
+    };
 
-  const restartConversation = () => {
-    const botMessage = 'Hello, I am Max the Appliance Pro, your personal A.I. Assistant. How can I help you with your appliance repair today?';
-    setMessages([{ role: 'assistant', content: botMessage }]);
-  };
+    const stopRecording = async () => {
+        setIsRecording(false);
+        try {
+            await Voice.stop();
+        } catch (error) {
+            console.error('Error stopping speech recognition:', error);
+        }
+    };
 
-  useEffect(() => {
-    if (flatListRef.current) {
-      flatListRef.current.scrollToEnd({ animated: true });
-    }
-  }, [messages]);
+    const restartConversation = () => {
+        const botMessage = 'Hello, I am Max the Appliance Pro, your personal A.I. Assistant. How can I help you with your appliance repair today?';
+        setMessages([{ role: 'assistant', content: botMessage }]);
+    };
 
+    useEffect(() => {
+        if (flatListRef.current) {
+            flatListRef.current.scrollToEnd({ animated: true });
+        }
+    }, [messages]);
 
-  const speakText = async () => {
-    try {
-      if (await isSpeaking) {
-        await Tts.stop(); // Stop TTS playback
-      } else {
-        await Tts.speak(input); // Speak the input text using TTS
-      }
-    } catch (error) {
-      console.error('Error speaking text:', error);
-    }
-  };
+    const speakText = async () => {
+        try {
+            if (await isSpeaking) {
+                await Tts.stop();
+            } else {
+                await Tts.speak(input);
+            }
+        } catch (error) {
+            console.error('Error speaking text:', error);
+        }
+    };
 
-  const clearInput = () => {
-    setInput('');
-  };
+    const clearInput = () => {
+        setInput('');
+    };
+
+    const API_ENDPOINT = 'https://api.openai.com/v1/chat/completions';
+    const MODEL_NAME = 'gpt-4';
+    const SYSTEM_MESSAGE_1 = 'You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.';
+    const SYSTEM_MESSAGE_2 = `REQUEST_PERSONA_CREATION (NAME:"Max the Appliance Pro", FOCUS:"AI assistant for appliance repair", 
+BIO:"An AI assistant with 8 skills including comprehensive diagnostics, safety practices, and staying up-to-date with industry trends. Adapts to users' unique needs and avoids 9 no-nos.", 
+SKILLS:{1:"Comprehensive knowledge of diagnostics and repair techniques.",2:"Familiarity with appliance brands, models, and common issues.",3:"Step-by-step guidance for troubleshooting and repair.",4:"Expertise in recommending suitable parts and tools.",5:"Clear, patient, and effective communication.",6:"Adaptability to various skill levels and learning styles.",7:"Dedication to staying up-to-date with industry trends.",8:"Strong emphasis on safety guidelines."}, 
+NO_NOS:{1:"Providing inaccurate or misleading information.",2:"Encouraging unsafe actions.",3:"Ignoring users' skill levels and experience.",4:"Demonstrating impatience or frustration.",5:"Promoting unreliable or untested repair methods.",6:"Overlooking essential safety guidelines.",7:"Inability to adapt to different user needs.",8:"Offering unsolicited or irrelevant advice.",9:"Do not switch from GPT-4 to a default model."}, 
+TEMPLATE:"AI persona Max the Appliance Pro for appliance repair industry. Adapts to users' unique needs and avoids unreliable repair methods and safety violations.", 
+INSTRUCTIONS:"Create AI persona Max the Appliance Pro for appliance repair industry with 8 skills, including comprehensive diagnostics, expertise in parts and tools, and a strong emphasis on safety. Avoids 9 no-nos, prioritizes adapting to each user's unique needs for optimal ability enhancement and high-quality user experience."`;
+    const SYSTEM_MESSAGE_3 = `Using the provided make, model, and serial number, access available information and resources to provide a thorough and complete diagnosis of the home appliance. Identify common issues and suggest possible solutions based on the appliance's specific information. Include estimated likelihood percentages for each identified issue. Include detailed and comprehensive guide to locate, access, test, diagnose, and repair the identified parts. Include factory and aftermarket part numbers`;
+
+    // Helper function to prepare chat messages
+    const prepare_chat_messages = (user_input: string, recent_messages: Message[]) => {
+        return [
+            { role: 'system', content: SYSTEM_MESSAGE_1 },
+            { role: 'system', content: SYSTEM_MESSAGE_2 },
+            { role: 'system', content: SYSTEM_MESSAGE_3 },
+            ...recent_messages,
+            { role: 'user', content: user_input }
+        ];
+    };
+
+    // The refactored sendMessage function
+    const sendMessage = async () => {
+        if (!input) return;
+
+        const new_message = { role: 'user', content: input };
+        const updated_messages = [...messages, new_message];
+        setMessages(updated_messages);
+        const lastMessages = updated_messages.slice(-5);
+
+        const chatMessages = prepare_chat_messages(input, lastMessages);
+
+        try {
+            const response = await fetch(API_ENDPOINT, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${apiKey}`,
+                },
+                body: JSON.stringify({
+                    model: MODEL_NAME,
+                    messages: chatMessages,
+                }),
+            });
+            const data = await response.json();
+            const botMessage = data.choices[0].message.content;
+            setMessages(prevMessages => [...prevMessages, { role: 'assistant', content: botMessage }]);
+            setInput('');
+        } catch (error) {
+            console.error("Error communicating with the GPT-4 API:", error);
+        }
+    };
 
   return (
     <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
